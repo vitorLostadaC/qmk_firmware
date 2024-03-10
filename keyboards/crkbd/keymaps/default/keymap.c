c50517e7156c068d8915dde80718f4e72dec6193
VitorLostadaC <vitorlostada@hotmail.com>
2024-03-10 07:42:19 -0300

    feat: back normla alt

--- keyboards/crkbd/keymaps/default/keymap.c
+++ keyboards/crkbd/keymaps/default/keymap.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,     KC_W,    KC_E,     KC_R,     KC_T,                       KC_Y,    KC_U,     KC_I,    KC_O,     KC_P,     KC_BSPC,
         KC_LCTL,   KC_A,     KC_S,    KC_D,     KC_F,     KC_G,                       KC_H,    KC_J,     KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
         KC_LSFT,    KC_Z,     KC_X,    KC_C,     KC_V,     KC_B,                       KC_N,    KC_M,     KC_COMM, KC_DOT,   KC_SLSH,  KC_LSFT,
-        LT(3,KC_LALT), OSL(2), KC_SPC,                                         KC_ENT, LT(1,KC_ESC), LGUI_T(KC_LGUI)
+        KC_LALT, OSL(2), KC_SPC,                                         KC_ENT, LT(1,KC_ESC), LGUI_T(KC_LGUI)
     ),
 
     [1] = LAYOUT_split_3x6_3(
